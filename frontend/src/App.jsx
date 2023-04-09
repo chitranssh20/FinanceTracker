5dc73b69e4fe34548e09c51e8c562b79e53af028
Chitranssh <saxenaayush2052001@gmail.com>
2023-04-09 15:04:28 +0530

    Created SideBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import ResponsiveSidebar from './ResponsiveSidebar'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Income from './Income';
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Router>
+        <Routes>
+
+          <Route path='/' element={<ResponsiveSidebar />} />
+          <Route path='/income' element={<>
+             <ResponsiveSidebar />
+             <Income />
+          </>
+             } />
+          <Route path='/expenses' element={<ResponsiveSidebar />} />
+          <Route path='/budget' element={<ResponsiveSidebar />} />
+          <Route path='/savings' element={<ResponsiveSidebar />} />
+          <Route path='/investments' element={<ResponsiveSidebar />} />
+          <Route path='/debt' element={<ResponsiveSidebar />} />
+          <Route path='/net-worth' element={<ResponsiveSidebar />} />
+          <Route path='/financial-goals' element={<ResponsiveSidebar />} />
+          <Route path='/reports' element={<ResponsiveSidebar />} />
+          <Route path='/alerts' element={<ResponsiveSidebar />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
